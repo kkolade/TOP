9f1b31660f399206a603567a3e6fc08965a15fed
kola kolade <kolakolade@gmail.com>
2024-02-28 20:56:21 +0100

    refactor object constructor to adding method to object prototype

--- JavaScript/book.js
+++ JavaScript/book.js
@@ -3,12 +3,13 @@ function Book(author, title, numPages, isRead) {
   this.title = title;
   this.numPages = numPages;
   this.isRead = isRead;
-  this.info = function () {
-    return `${title} by ${author}, ${numPages} pages, ${
-      isRead ? 'already read' : 'not read yet'
-    }`;
-  };
 }
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.numPages} pages, ${
+    this.isRead ? 'already read' : 'not read yet'
+  }`;
+};
 
 const theGoodLife = new Book('Leke Kolade', 'The Good Life', 153, true);
 
